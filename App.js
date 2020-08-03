faf247a5eb3ccc4f4c73050fb22aa36edc373c41
Bingotop <wodnjs3600@naver.com>
2020-08-04 00:37:39 +0900

    2020/08/04

---
+++ App.js
@@ -0,0 +1,55 @@
+
+import React,{useState} from 'react';
+import {View,Text,StyleSheet,FlatList,Alert} from "react-native";
+import Header from "./component/Header";
+import uuid from "uuid-random";
+import ListItem from "./component/ListItem";
+import AddItem from "./component/Additem";
+
+const App = ()=> {
+  const [items,setItems] = useState([
+    {id:uuid(),text:"Milk"},
+    {id:uuid(),text:"Eggs"},
+    {id:uuid(),text:"Bread"},
+    {id:uuid(),text:"Juice"},
+  ]);
+  
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item=> item.id != id);
+    });
+  };
+
+  const addItem = (text)=> {
+    if (!text){
+      Alert.alert("Error","Please enter an item");
+    }else{
+
+    }
+
+    setItems(prevItems => {
+      return [{id:uuid(),text},...prevItems];
+    });
+  }
+  return(
+    <View style={styles.container}>
+      <Header/>
+      <AddItem addItem={addItem}/>
+      <FlatList 
+      data={items} 
+      renderItem={({item})=> 
+        <ListItem item={item}
+        deleteItem={deleteItem}
+         />}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+});
+
+export default App;
